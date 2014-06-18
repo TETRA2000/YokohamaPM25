a2cc9c0003314d52ca983af7095fd533f95687af
TETRA2000 <sagittariusm25@gmail.com>
2014-06-19 01:06:06 +0900

    iOSプロジェクトを作成

---
+++ iOS/YokohamaPM25/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YokohamaPM25
+//
+//  Created by takahikoinayama on 2014/06/19.
+//  Copyright (c) 2014年 TETRA2000. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
